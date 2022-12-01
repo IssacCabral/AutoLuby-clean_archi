7fba20ad37707403b365ce9392dfaf24079d8461
Issac Cabral <issaccabrall@gmail.com>
2022-12-01 10:23:21 -0300

    chore: add Jest and typescript configs

---
+++ src/my-test.spec.ts
@@ -0,0 +1,5 @@
+describe('My Tests', () => {
+  test('Should 1 + 1 equal 2', () => {
+    expect(1+1).toBe(2)
+  })
+})
---
+++ src/script.ts
@@ -0,0 +1,6 @@
+console.log('Olá')
+
+const PORT = 3000
+
+console.log(PORT)
+console.log('Eae amegan. Tudo bem com você?')
