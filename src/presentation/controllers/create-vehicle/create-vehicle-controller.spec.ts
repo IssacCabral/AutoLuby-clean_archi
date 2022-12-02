e2a66780113fb0f9a0041c0d6d258b20d23f08c4
Issac Cabral <issaccabrall@gmail.com>
2022-12-01 21:16:50 -0300

    test: ensure CreateVehicleController returns 201 if valid data is provided

--- src/presentation/controllers/create-vehicle/create-vehicle-controller.spec.ts
+++ src/presentation/controllers/create-vehicle/create-vehicle-controller.spec.ts
@@ -1,14 +1,14 @@
-import { VehicleModel } from '@domain/models/vehicle'
-import { CreateVehicleParams } from '@domain/types/create-vehicle-params'
-import { ICreateVehicle } from '@domain/usecases/create-vehicle'
-import { MissingParamError } from '@errors/missing-param-error'
-import { ServerError } from '@errors/server-error'
-import { HttpRequest } from '@protocols/http'
-import {CreateVehicleController} from './create-vehicle-controller'
+import { VehicleModel } from "@domain/models/vehicle";
+import { CreateVehicleParams } from "@domain/types/create-vehicle-params";
+import { ICreateVehicle } from "@domain/usecases/create-vehicle";
+import { MissingParamError } from "@errors/missing-param-error";
+import { ServerError } from "@errors/server-error";
+import { HttpRequest } from "@protocols/http";
+import { CreateVehicleController } from "./create-vehicle-controller";
 
 const makeFakeCreateRequest = (): HttpRequest => {
   return {
-    body:{
+    body: {
       brand: "any_brand",
       chassis: "any_chassis",
       color: "any_color",
@@ -18,23 +18,23 @@ const makeFakeCreateRequest = (): HttpRequest => {
       model: "any_model",
       status: "any_status",
       year: 2000,
-    }
+    },
   };
 };
 
 const makeCreateVehicle = (): ICreateVehicle => {
-  class CreateVehicleStub implements ICreateVehicle{
+  class CreateVehicleStub implements ICreateVehicle {
     async create(params: CreateVehicleParams): Promise<Error | VehicleModel> {
       const fakeVehicle: VehicleModel = {
-        id: "any_id",
-        brand: "any_brand",
-        chassis: "any_chassis",
-        color: "any_color",
+        id: "valid_id",
+        brand: "valid_brand",
+        chassis: "valid_chassis",
+        color: "valid_color",
         cost_price: 0,
         sale_price: 0,
         km: 0,
-        model: "any_model",
-        status: "any_status",
+        model: "valid_model",
+        status: "valid_status",
         year: 2000,
         createdAt: new Date(2022, 9, 1),
         updatedAt: new Date(2022, 9, 1),
@@ -42,202 +42,239 @@ const makeCreateVehicle = (): ICreateVehicle => {
       return fakeVehicle;
     }
   }
-  return new CreateVehicleStub()
-}
+  return new CreateVehicleStub();
+};
 
-interface SutTypes{
-  sut: CreateVehicleController
-  createVehicleStub: ICreateVehicle
+interface SutTypes {
+  sut: CreateVehicleController;
+  createVehicleStub: ICreateVehicle;
 }
 
 const makeSut = (): SutTypes => {
-  const createVehicleStub = makeCreateVehicle()
-  const sut = new CreateVehicleController(createVehicleStub)
+  const createVehicleStub = makeCreateVehicle();
+  const sut = new CreateVehicleController(createVehicleStub);
   return {
     sut,
-    createVehicleStub
-  }
-}
+    createVehicleStub,
+  };
+};
 
-describe('CreateVehicle Controller', () => {
-  test('Should return 400 if no model is provided', async () => {
-    const {sut} = makeSut()
+describe("CreateVehicle Controller", () => {
+  test("Should return 400 if no model is provided", async () => {
+    const { sut } = makeSut();
     const httpRequest = {
       body: {
-        brand: "any_brand",
-        chassis: "any_chassis",
-        color: "any_color",
+        brand: "valid_brand",
+        chassis: "valid_chassis",
+        color: "valid_color",
         cost_price: 0,
         sale_price: 0,
         km: 0,
-        status: "any_status",
+        status: "valid_status",
         year: 2000,
-      }
-    }
-    const httpResponse = await sut.handle(httpRequest)
-    expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new MissingParamError('model'))
-  })
+      },
+    };
+    const httpResponse = await sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new MissingParamError("model"));
+  });
 
-  test('Should return 400 if no brand is provided', async () => {
-    const {sut} = makeSut()
+  test("Should return 400 if no brand is provided", async () => {
+    const { sut } = makeSut();
     const httpRequest = {
       body: {
-        chassis: "any_chassis",
-        color: "any_color",
+        chassis: "valid_chassis",
+        color: "valid_color",
         cost_price: 0,
         sale_price: 0,
         km: 0,
-        model: "any_model",
-        status: "any_status",
+        model: "valid_model",
+        status: "valid_status",
         year: 2000,
-      }
-    }
-    const httpResponse = await sut.handle(httpRequest)
-    expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new MissingParamError('brand'))
-  })
+      },
+    };
+    const httpResponse = await sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new MissingParamError("brand"));
+  });
 
-  test('Should return 400 if no status is provided', async () => {
-    const {sut} = makeSut()
+  test("Should return 400 if no status is provided", async () => {
+    const { sut } = makeSut();
     const httpRequest = {
       body: {
-        brand: "any_brand",
-        chassis: "any_chassis",
-        color: "any_color",
+        brand: "valid_brand",
+        chassis: "valid_chassis",
+        color: "valid_color",
         cost_price: 0,
         sale_price: 0,
         km: 0,
-        model: "any_model",
+        model: "valid_model",
         year: 2000,
-      }
-    }
-    const httpResponse = await sut.handle(httpRequest)
-    expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new MissingParamError('status'))
-  })
+      },
+    };
+    const httpResponse = await sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new MissingParamError("status"));
+  });
 
-  test('Should return 400 if no year is provided', async () => {
-    const {sut} = makeSut()
+  test("Should return 400 if no year is provided", async () => {
+    const { sut } = makeSut();
     const httpRequest = {
       body: {
-        brand: "any_brand",
-        chassis: "any_chassis",
-        color: "any_color",
+        brand: "valid_brand",
+        chassis: "valid_chassis",
+        color: "valid_color",
         cost_price: 0,
         sale_price: 0,
         km: 0,
-        model: "any_model",
-        status: "any_status"
-      }
-    }
-    const httpResponse = await sut.handle(httpRequest)
-    expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new MissingParamError('year'))
-  })
+        model: "valid_model",
+        status: "valid_status",
+      },
+    };
+    const httpResponse = await sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new MissingParamError("year"));
+  });
 
-  test('Should return 400 if no km is provided', async () => {
-    const {sut} = makeSut()
+  test("Should return 400 if no km is provided", async () => {
+    const { sut } = makeSut();
     const httpRequest = {
       body: {
-        brand: "any_brand",
-        chassis: "any_chassis",
-        color: "any_color",
+        brand: "valid_brand",
+        chassis: "valid_chassis",
+        color: "valid_color",
         cost_price: 0,
         sale_price: 0,
-        model: "any_model",
-        status: "any_status",
+        model: "valid_model",
+        status: "valid_status",
         year: 2000,
-      }
-    }
-    const httpResponse = await sut.handle(httpRequest)
-    expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new MissingParamError('km'))
-  })
+      },
+    };
+    const httpResponse = await sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new MissingParamError("km"));
+  });
 
-  test('Should return 400 if no color is provided', async () => {
-    const {sut} = makeSut()
+  test("Should return 400 if no color is provided", async () => {
+    const { sut } = makeSut();
     const httpRequest = {
       body: {
-        brand: "any_brand",
-        chassis: "any_chassis",
-        cost_price: 0,
-        sale_price: 0,
-        km: 0,
-        model: "any_model",
-        status: "any_status",
-        year: 2000,
-      }
-    }
-    const httpResponse = await sut.handle(httpRequest)
-    expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new MissingParamError('color'))
-  })
-
-  test('Should return 400 if no chassis is provided', async () => {
-    const {sut} = makeSut()
-    const httpRequest = {
-      body: {
-        brand: "any_brand",
-        color: "any_color",
+        brand: "valid_brand",
+        chassis: "valid_chassis",
         cost_price: 0,
         sale_price: 0,
         km: 0,
-        model: "any_model",
-        status: "any_status",
+        model: "valid_model",
+        status: "valid_status",
         year: 2000,
-      }
-    }
-    const httpResponse = await sut.handle(httpRequest)
-    expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new MissingParamError('chassis'))
-  })
+      },
+    };
+    const httpResponse = await sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new MissingParamError("color"));
+  });
 
-  test('Should return 400 if no sale_price is provided', async () => {
-    const {sut} = makeSut()
+  test("Should return 400 if no chassis is provided", async () => {
+    const { sut } = makeSut();
     const httpRequest = {
       body: {
-        brand: "any_brand",
-        chassis: "any_chassis",
-        color: "any_color",
+        brand: "valid_brand",
+        color: "valid_color",
         cost_price: 0,
-        km: 0,
-        model: "any_model",
-        status: "any_status",
-        year: 2000,
-      }
-    }
-    const httpResponse = await sut.handle(httpRequest)
-    expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new MissingParamError('sale_price'))
-  })
-
-  test('Should return 400 if no cost_price is provided', async () => {
-    const {sut} = makeSut()
-    const httpRequest = {
-      body: {
-        brand: "any_brand",
-        chassis: "any_chassis",
-        color: "any_color",
         sale_price: 0,
         km: 0,
-        model: "any_model",
-        status: "any_status",
+        model: "valid_model",
+        status: "valid_status",
         year: 2000,
-      }
-    }
-    const httpResponse = await sut.handle(httpRequest)
-    expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new MissingParamError('cost_price'))
-  })
+      },
+    };
+    const httpResponse = await sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new MissingParamError("chassis"));
+  });
 
-  test('Should return 500 if CreateVehicle throws', async () => {
-    const {sut, createVehicleStub} = makeSut()
-    jest.spyOn(createVehicleStub, 'create').mockImplementationOnce(() => {
-      throw new Error()
-    })
-    const httpResponse = await sut.handle(makeFakeCreateRequest())
-    expect(httpResponse.statusCode).toBe(500)
-    expect(httpResponse.body).toEqual(new ServerError())
-  })
-})
+  test("Should return 400 if no sale_price is provided", async () => {
+    const { sut } = makeSut();
+    const httpRequest = {
+      body: {
+        brand: "valid_brand",
+        chassis: "valid_chassis",
+        color: "valid_color",
+        cost_price: 0,
+        km: 0,
+        model: "valid_model",
+        status: "valid_status",
+        year: 2000,
+      },
+    };
+    const httpResponse = await sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new MissingParamError("sale_price"));
+  });
+
+  test("Should return 400 if no cost_price is provided", async () => {
+    const { sut } = makeSut();
+    const httpRequest = {
+      body: {
+        brand: "valid_brand",
+        chassis: "valid_chassis",
+        color: "valid_color",
+        sale_price: 0,
+        km: 0,
+        model: "valid_model",
+        status: "valid_status",
+        year: 2000,
+      },
+    };
+    const httpResponse = await sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new MissingParamError("cost_price"));
+  });
+
+  test("Should return 500 if CreateVehicle throws", async () => {
+    const { sut, createVehicleStub } = makeSut();
+    jest.spyOn(createVehicleStub, "create").mockImplementationOnce(() => {
+      throw new Error();
+    });
+    const httpResponse = await sut.handle(makeFakeCreateRequest());
+    expect(httpResponse.statusCode).toBe(500);
+    expect(httpResponse.body).toEqual(new ServerError());
+  });
+
+  test("Should call CreateVehicle with correct values", async () => {
+    const { sut, createVehicleStub } = makeSut();
+    const createSpy = jest.spyOn(createVehicleStub, "create");
+    await sut.handle(makeFakeCreateRequest());
+    expect(createSpy).toHaveBeenCalledWith({
+      brand: "any_brand",
+      chassis: "any_chassis",
+      color: "any_color",
+      cost_price: 0,
+      sale_price: 0,
+      km: 0,
+      model: "any_model",
+      status: "any_status",
+      year: 2000,
+    });
+  });
+
+  test("Should return 201 if valid data is provided", async () => {
+    const { sut } = makeSut();
+    const httpRequest = {
+      body: {
+        brand: "valid_brand",
+        chassis: "valid_chassis",
+        color: "valid_color",
+        cost_price: 0,
+        sale_price: 0,
+        km: 0,
+        model: "valid_model",
+        status: "valid_status",
+        year: 2000,
+      },
+    };
+    const httpResponse = await sut.handle(makeFakeCreateRequest());
+    expect(httpResponse.statusCode).toBe(201);
+    expect(httpResponse.body).toMatchObject(httpRequest.body)
+  });
+});
