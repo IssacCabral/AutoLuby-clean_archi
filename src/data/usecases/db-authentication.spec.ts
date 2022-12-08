4a6a092387273854e4359f05a202272db7999366
Issac Cabral <issaccabrall@gmail.com>
2022-12-08 15:16:52 -0300

    feat: ensure DbAuthentication call UpdateAccessTokenRepository with correct values

--- src/data/usecases/db-authentication.spec.ts
+++ src/data/usecases/db-authentication.spec.ts
@@ -1,6 +1,7 @@
 import { IFindUserByEmailRepository } from "@data/protocols/find-user-by-email-repository"
 import { IHashComparer } from "@data/protocols/hash-comparer"
 import { ITokenGenerator } from "@data/protocols/token-generator"
+import { IUpdateAccessTokenRepository } from "@data/protocols/update-access-token-repository"
 import { UserModel } from "@domain/models/user"
 import { InvalidCredentialsError } from "@errors/invalid-credentials-error"
 import { DbAuthentication } from "./db-authentication"
@@ -53,23 +54,40 @@ const makeTokenGenerator = (): ITokenGenerator => {
   return new TokenGeneratorStub()
 }
 
+const makeUpdateAccessTokenRepository = (): IUpdateAccessTokenRepository => {
+  class UpdateAccessTokenRepositoryStub implements IUpdateAccessTokenRepository{
+    async update(id: string, token: string): Promise<void>{
+      
+    }
+  }
+  return new UpdateAccessTokenRepositoryStub()
+}
+
 interface SutTypes{
   sut: DbAuthentication
   findUserByEmailRepositoryStub: IFindUserByEmailRepository
   hashComparerStub: IHashComparer
   tokenGeneratorStub: ITokenGenerator
+  updateAccessTokenRepositoryStub: IUpdateAccessTokenRepository
 }
 
 const makeSut = (): SutTypes => {
   const findUserByEmailRepositoryStub = makeFindUserByEmailRepository()
   const hashComparerStub = makeHashComparer()
   const tokenGeneratorStub = makeTokenGenerator()
-  const sut = new DbAuthentication(findUserByEmailRepositoryStub, hashComparerStub, tokenGeneratorStub)
+  const updateAccessTokenRepositoryStub = makeUpdateAccessTokenRepository()
+  const sut = new DbAuthentication(
+    findUserByEmailRepositoryStub, 
+    hashComparerStub, 
+    tokenGeneratorStub,
+    updateAccessTokenRepositoryStub
+  )
   return {
     sut,
     findUserByEmailRepositoryStub,
     hashComparerStub,
-    tokenGeneratorStub
+    tokenGeneratorStub,
+    updateAccessTokenRepositoryStub
   }
 }
 
@@ -143,5 +161,13 @@ describe('DbAuthentication UseCase', () => {
     const authParams = makeFakeAuthRequest()
     const result = await sut.auth(authParams.email, authParams.password)
     expect(result).toBe('any_token')
+  })
+
+  test('Should call UpdateAccessTokenRepository with correct values', async () => {
+    const {sut, updateAccessTokenRepositoryStub} = makeSut()
+    const updateSpy = jest.spyOn(updateAccessTokenRepositoryStub, 'update')
+    const authParams = makeFakeAuthRequest()
+    await sut.auth(authParams.email, authParams.password)
+    expect(updateSpy).toHaveBeenCalledWith('valid_id', 'any_token')
   })
 })
--- src/data/usecases/db-authentication.ts
+++ src/data/usecases/db-authentication.ts
@@ -1,6 +1,7 @@
 import { IFindUserByEmailRepository } from "@data/protocols/find-user-by-email-repository";
 import { IHashComparer } from "@data/protocols/hash-comparer";
 import { ITokenGenerator } from "@data/protocols/token-generator";
+import { IUpdateAccessTokenRepository } from "@data/protocols/update-access-token-repository";
 import { IAuthentication } from "@domain/usecases/authentication";
 import { InvalidCredentialsError } from "@errors/invalid-credentials-error";
 
@@ -8,7 +9,8 @@ export class DbAuthentication implements IAuthentication{
   constructor(
     private readonly findUserByEmailRepository: IFindUserByEmailRepository,
     private readonly hashComparer: IHashComparer,
-    private readonly tokenGenerator: ITokenGenerator
+    private readonly tokenGenerator: ITokenGenerator,
+    private readonly updateAccessTokenRepository: IUpdateAccessTokenRepository
   ) {}
 
   async auth(email: string, password: string): Promise<string | Error> {
@@ -22,7 +24,10 @@ export class DbAuthentication implements IAuthentication{
       return new InvalidCredentialsError()
     }
 
+    
     const accessToken = await this.tokenGenerator.generate(user.id)
+    await this.updateAccessTokenRepository.update(user.id, accessToken)
+
     return accessToken
   }
 }
