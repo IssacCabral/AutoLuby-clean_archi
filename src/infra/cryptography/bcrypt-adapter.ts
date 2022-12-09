4a7d0293d4d801492e8b755b67801cac5ac46a62
Issac Cabral <issaccabrall@gmail.com>
2022-12-09 17:07:25 -0300

    feat: make BCryptAdapter implement IHashComparer

--- src/infra/cryptography/bcrypt-adapter.ts
+++ src/infra/cryptography/bcrypt-adapter.ts
@@ -1,10 +1,15 @@
+import { IHashComparer } from "@data/protocols/cryptography/hash-comparer";
 import { IHasher } from "@data/protocols/cryptography/hasher";
 import bcrypt from 'bcrypt'
 
-export class BcryptAdapter implements IHasher{
+export class BcryptAdapter implements IHasher, IHashComparer{
   constructor(
     private readonly salt: number
   ) {}
+  
+  async compare(value: string, hash: string): Promise<boolean> {
+    return bcrypt.compare(value, hash)
+  }
 
   async hash(value: string): Promise<string> {
     const hash = await bcrypt.hash(value, this.salt)
